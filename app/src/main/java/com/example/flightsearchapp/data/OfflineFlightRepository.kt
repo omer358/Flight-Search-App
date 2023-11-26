07e56574e54542e69a2ba556fec79617ee3641a1
omer358 <omermaki358@gmail.com>
2023-11-26 15:23:33 +0200

    fix the error with LIKE clues in repo

--- app/src/main/java/com/example/flightsearchapp/data/OfflineFlightRepository.kt
+++ app/src/main/java/com/example/flightsearchapp/data/OfflineFlightRepository.kt
@@ -1,14 +1,17 @@
 package com.example.flightsearchapp.data
 
+import android.util.Log
 import com.example.flightsearchapp.data.dao.AirPortDao
 import com.example.flightsearchapp.data.entities.AirPort
 import kotlinx.coroutines.flow.Flow
 
+private const val TAG = "OfflineFlightRepository"
 class OfflineFlightRepository(
     private val airPortDao: AirPortDao,
 ) : FlightRepository {
     override fun searchForAirport(name: String): Flow<List<AirPort>> {
-        return airPortDao.findAirPorts(name)
+        Log.i(TAG, name)
+        return airPortDao.findAirPorts("%$name%")
     }
 
     override fun getAllAirPorts(): Flow<List<AirPort>> {
